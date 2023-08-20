4e5bd689c6dc3dc67f562ada577e992fc5115bb1
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-20 10:54:54 -0300

    Added service to search movies and examples to test. Added simple css for the page. Deleted vite base files.

--- practice-projects/projects/01-movie-searcher/src/App.jsx
+++ practice-projects/projects/01-movie-searcher/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import searchMovies from './services/movies';
+import './assets/app.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [search, setSearch] = useState('');
+	const [movies, setMovies] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleSubmit = async () => {
+		const movies = await searchMovies(search);
+		console.log(movies);
+		setMovies(movies);
+	}
+
+	return (
+		<main className="searcher">
+			<h1 className="searcher-title">Movie Searcher</h1>
+			<form action='#' className="searcher-form" onSubmit={handleSubmit}>
+				<input onChange={e => setSearch(e.target.value)} className="form-input" type="text" placeholder="Batman, Superman, Spiderman..." />
+				<button className="form-btn">Search</button>
+			</form>
+			<section className='movies'>
+				{movies
+					? movies.map(
+						(m, i) => {
+
+						}
+					)
+					: "No movies"}
+			</section>
+		</main>
+	)
 }
 
 export default App
---
+++ practice-projects/projects/01-movie-searcher/src/services/movies.js
@@ -0,0 +1,14 @@
+const API_KEY = 'f5e92244';
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+
+async function searchMovies(input) {
+	if (input === '') return null;
+
+	const search = `&s=${input}`;
+	const movies = await fetch(API_URL + search);
+	const moviesJson = await movies.json();
+
+	return moviesJson.Search;
+}
+
+export default searchMovies;
