4d851cd00d37229dc706ab33b9126824c7d537bd
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-20 11:31:12 -0300

    Prevent double search on the same input.

--- practice-projects/projects/01-movie-searcher/src/App.jsx
+++ practice-projects/projects/01-movie-searcher/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import searchMovies from './services/movies';
 import ListOfMovies from './components/ListOfMovies';
 import batman from './examples/batman.json'
@@ -7,16 +7,21 @@ import './assets/app.css'
 function App() {
 	const [search, setSearch] = useState('');
 	const [movies, setMovies] = useState(null);
+	const prevSearch = useRef('');
 
 	const handleSubmit = async () => {
-		// const movies = await searchMovies(search);
-		setMovies(batman.Search);
+		if (prevSearch.current === search) return;
+
+		const movies = await searchMovies(search);
+		prevSearch.current = search;
+		setMovies(movies);
+		// setMovies(batman.Search);
 	}
 
 	return (
 		<main className="searcher">
 			<h1 className="searcher-title">Movie Searcher</h1>
-			<form action='#' className="searcher-form" onSubmit={handleSubmit}>
+			<form className="searcher-form" onSubmit={handleSubmit}>
 				<input onChange={e => setSearch(e.target.value)} className="form-input" type="text" placeholder="Batman, Superman, Spiderman..." />
 				<button className="form-btn">Search</button>
 			</form>
