aa6cf89d6ff5656d611403e8a823ba29aeac055d
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-21 15:11:58 -0300

    Changed alt on imgs.

--- practice-projects/projects/01-movie-searcher/src/components/Movie.jsx
+++ practice-projects/projects/01-movie-searcher/src/components/Movie.jsx
@@ -2,7 +2,7 @@ function Movie({ title, year, imgUrl }) {
 	return (
 		<>
 			<h3 className="movie-title">{title}</h3>
-			<img className="movie-img" src={imgUrl} alt={`Image for the search: ${title}`} />
+			<img className="movie-img" src={imgUrl} alt={`Image for the title: ${title}`} />
 			<p className="movie-year">{year}</p>
 		</>
 	);
