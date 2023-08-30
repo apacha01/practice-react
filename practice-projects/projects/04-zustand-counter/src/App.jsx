58e58ffb4f7984803a90c4a8a66ddb09894e09c8
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-30 16:30:37 -0300

    Install zustand and create counter store. Implement counter.

--- practice-projects/projects/04-zustand-counter/src/App.jsx
+++ practice-projects/projects/04-zustand-counter/src/App.jsx
@@ -1,14 +1,19 @@
+import useCounterStore from "./stores/counterStore";
+
 function App() {
 	let img = "https://raw.githubusercontent.com/pmndrs/zustand/main/bear.jpg";
+	const count = useCounterStore(state => state.count);
+	const increment = useCounterStore(state => state.inc);
+	const decrement = useCounterStore(state => state.dec);
 
 	return (
 		<main className="counter">
 			<h1>Zustand Counter</h1>
 			<img className="zustand-img" src={img} alt="Zustand Bear" />
 			<div className="counter-info">
-				<button className="add-btn">+</button>
-				<p className="count">0</p>
-				<button className="sub-btn">-</button>
+				<button className="add-btn" onClick={increment} >+</button>
+				<p className="count">{count}</p>
+				<button className="sub-btn" onClick={decrement} >-</button>
 			</div>
 		</main>
 	)
