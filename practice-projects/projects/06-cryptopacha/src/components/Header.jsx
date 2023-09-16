f887b3e058d26c1cf7fe4fa78fb035011e3d115f
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-16 19:13:10 -0300

    Created Header component.

---
+++ practice-projects/projects/06-cryptopacha/src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header () {
+	return (
+		<header className="header">
+			<nav className="menu-container">
+				<ul className="menu">
+					<li className="menu-item">
+						<a href="#" className="menu-link">Home</a>
+					</li>
+					<li className="menu-item">
+						<a href="#" className="menu-link">Markets</a>
+					</li>
+					<li className="menu-item">
+						<a href="#" className="menu-link">About Us</a>
+					</li>
+					<li className="menu-item">
+						<a href="#" className="menu-link">Follow Us</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
