939f8b81ec92237fe6ca4c24995a77c45a8ec596
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-20 19:11:36 -0300

    Removed lorems, added footer and changed hero button style.

--- practice-projects/projects/06-cryptopacha/src/App.jsx
+++ practice-projects/projects/06-cryptopacha/src/App.jsx
@@ -1,5 +1,6 @@
 import About from './components/About';
 import Follow from './components/Follow';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Market from './components/Market';
@@ -12,16 +13,7 @@ function App() {
 			<Market />
 			<About />
 			<Follow />
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nisi dolor dignissimos. Eos, nisi facere laudantium cumque rerum provident quia ipsa voluptate omnis, dolorum rem tempora necessitatibus id praesentium aperiam.
-			Illo voluptatem sed veniam, unde maiores officia est beatae dolore quidem? Inventore iure reiciendis minus modi nobis provident voluptatibus quae cumque perspiciatis. Aut possimus excepturi quis atque quibusdam tenetur fuga?
-			Accusamus magnam laudantium nulla temporibus dolore. Ipsam commodi sapiente neque ullam hic. Quod illum molestias rem aliquam est quibusdam consequatur tempora illo blanditiis doloribus aspernatur laudantium, dolor fugiat. Nobis, magnam.
-			Alias velit possimus, perferendis sunt quibusdam laudantium et error excepturi minima, temporibus modi labore eius inventore quod neque, aliquid exercitationem soluta quos veniam itaque reiciendis sed? Delectus ab nisi temporibus?
-			Aspernatur quasi vero error! Delectus modi totam officiis eaque corporis possimus labore neque eum provident fuga natus a rerum temporibus consectetur incidunt quae doloribus asperiores, excepturi ut! Eaque, impedit cupiditate?
-			Soluta non velit, quidem laboriosam inventore ea recusandae. Illo error obcaecati quas necessitatibus amet quibusdam ducimus tempore quam mollitia quo reiciendis maiores labore nobis magnam blanditiis iste, culpa cumque. Adipisci?
-			Dolores non saepe autem sit molestiae nesciunt repellendus quam perspiciatis ab dolore sint eveniet, eaque fugiat nulla optio sequi inventore deserunt tempore recusandae dicta sunt laudantium reprehenderit? Excepturi, enim ea!
-			Porro hic totam doloribus tenetur quasi atque. Id odit quaerat omnis quis temporibus voluptatem consectetur optio ab, eum vitae deserunt recusandae quae voluptatibus atque ut, non ducimus magni, deleniti et?
-			Vero officiis nihil vitae, quos eligendi voluptatum architecto, qui odit a cum ipsum iusto natus saepe nobis quaerat ex sint voluptatibus ducimus voluptate voluptatem nostrum, corrupti inventore ea totam. Totam!
-			Itaque molestias cupiditate esse nulla cumque expedita in voluptates recusandae deleniti accusamus! Quod labore quo possimus nulla recusandae ea numquam, molestias placeat laborum atque delectus harum, optio at pariatur libero!
+			<Footer />
 		</>
 	);
 }
---
+++ practice-projects/projects/06-cryptopacha/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer () {
+	return (
+		<footer className="pt-28 w-full bg-dark-purple text-white">
+			<ul className="flex items-center justify-center gap-8">
+				<li className="text-sm opacity-90">Terms & Condition</li>
+				<li className="text-sm opacity-90">Privacy</li>
+				<li className="text-sm opacity-90">Security</li>
+				<li className="text-sm opacity-90">Copyright 2023 - Cryptopacha Co.</li>
+			</ul>
+		</footer>
+	);
+}
+
+export default Footer;
