befff26895138b862fc4b4027d09e2acc409ea44
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-09-20 19:57:15 -0300

    Changed from promises to await. Added function to format the data.

--- practice-projects/projects/06-cryptopacha/src/services/cryptos.js
+++ practice-projects/projects/06-cryptopacha/src/services/cryptos.js
@@ -5,11 +5,31 @@ const PAGINATED_URL = (pp, p) => `markets?vs_currency=usd&order=market_cap_desc&
 const COIN_ID_URL = (id) => `${id}?tickers=false&community_data=false&developer_data=false`;
 
 const getCoinById = async (id) => {
-	return await fetch(COINGECKO_API_URL + COIN_ID_URL(id)).then(res => res.json);
+	const data = await fetch(COINGECKO_API_URL + COIN_ID_URL(id));
+	const json = data.json();
+	return json;
 };
 
 const getCoinsPaginated = async (perpage, page) => {
-	return await fetch(COINGECKO_API_URL + PAGINATED_URL(perpage, page)).then(res => res.json);
+	const data = await fetch(COINGECKO_API_URL + PAGINATED_URL(perpage, page));
+	const json = await data.json();
+	return json;
 };
 
-export { getCoinById, getCoinsPaginated };
+
+// Regex for format from: https://stackoverflow.com/questions/721304/insert-commas-into-number-string
+const formatCoins = (coins) => {
+	return coins.map(c => {
+		return {
+			name: c.name,
+			// Avoid inseting commas after decimal point
+			currentPrice: c.current_price.toString().indexOf('.') != -1
+				? c.current_price.toString().replace(/\d{1,3}(?=(\d{3})+(?=\.))/g, '$&,')
+				: c.current_price.toString().replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, '$&,'),
+			priceChange24h: c.price_change_percentage_24h.toFixed(2),
+			marketCap: 	c.market_cap.toString().replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, '$&,')
+		};
+	});
+};
+
+export { getCoinById, getCoinsPaginated, formatCoins };
