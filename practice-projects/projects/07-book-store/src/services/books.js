ef62ea770d8110808dc8a956c0bcdcd4703b49b6
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-02 13:45:53 -0300

    Refactor filter function.

--- practice-projects/projects/07-book-store/src/services/books.js
+++ practice-projects/projects/07-book-store/src/services/books.js
@@ -21,24 +21,12 @@ const getAllBooks = async () => {
 const getFilteredBooks = (books, filters = {}) => {
 	return books.filter(b => {
 
-		if (filters.isbn !== undefined) {
-			if (!b.isbn.localeCompare(filters.isbn)) return false;
-		}
-		if (filters.author !== undefined) {
-			if (!b.author?.name.localeCompare(filters.author.name)) return false;
-		}
-		if (filters.genre !== undefined) {
-			if (b.genre !== filters.genre && filters.genre !== DEFAULT_GENRE_TEXT) return false;
-		}
-		if (filters.pages !== undefined) {
-			if (!(b.pages >= filters.pages.min && b.pages <= filters.pages.max)) return false;
-		}
-		if (filters.title !== undefined) {
-			if (!b.title.contains(filters.title)) return false;
-		}
-		if (filters.year !== undefined) {
-			if (!(b.year >= filters.year.min && b.year <= filters.year.max)) return false;
-		}
+		if (filters.isbn?.localeCompare(b.isbn)) return false;
+		if (filters.author?.name.localeCompare(b.author.name)) return false;
+		if (filters.genre?.localeCompare(b.genre) && filters.genre?.localeCompare(DEFAULT_GENRE_TEXT)) return false;
+		if (filters.pages !== undefined && !(b.pages >= filters.pages.min && b.pages <= filters.pages.max)) return false;
+		if (filters.title?.contains(b.title)) return false;
+		if (filters.year !== undefined && !(b.year >= filters.year.min && b.year <= filters.year.max)) return false;
 
 		return true;
 	});
