8f25669de40d32c307057b8cf24ad5735c63214e
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-07 16:19:30 -0300

    Change tailwind build path into src. Add base for deploying. Change some package.json scripts for preview.

--- practice-projects/projects/07-book-store/vite.config.js
+++ practice-projects/projects/07-book-store/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/reading-list-interview/',
 	test: {
 		environment: 'jsdom',
 		setupFiles: './src/__tests__/setup.js'
