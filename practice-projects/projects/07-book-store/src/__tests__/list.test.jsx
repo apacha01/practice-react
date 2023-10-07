98c4f4a18ef6cef288fa539385c8dafff1e6a0e3
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-07 12:36:25 -0300

    Changed from fireEvent to userEvent as docs say. Implemented test for search bar filter.

--- practice-projects/projects/07-book-store/src/__tests__/list.test.jsx
+++ practice-projects/projects/07-book-store/src/__tests__/list.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 import App from '../App.jsx';
 
@@ -19,7 +20,7 @@ describe('reading list book tests', async () => {
 		expect(screen.getByRole('reading-list').innerHTML).not.toContain('El Señor de los Anillos');
 
 		// press one (the first) button
-		fireEvent.click(buttons[0]);
+		await userEvent.click(buttons[0]);
 
 		// check updated counters
 		expect(screen.getByRole('available-books-counter').innerHTML).toBe('12');
@@ -46,7 +47,7 @@ describe('reading list book tests', async () => {
 		expect(screen.getByRole('reading-list').innerHTML).toContain('El Señor de los Anillos');
 
 		// press one (the first) button
-		fireEvent.click(buttons[0]);
+		await userEvent.click(buttons[0]);
 
 		// check updated counters
 		expect(screen.getByRole('available-books-counter').innerHTML).toBe('13');
@@ -57,14 +58,38 @@ describe('reading list book tests', async () => {
 	});
 });
 
-describe('available books filters test', async () => {
+describe('available books filters test', () => {
+	it('types in the search bar an author', async () => {
+		render(<App />);
+
+		const searchBar = screen.getByLabelText('Title / Author / ISBN');
+
+		await userEvent.type(searchBar, 'George');
+
+		// check counter
+		expect(screen.getByRole('available-books-counter').innerHTML).toBe('2');
+		//check the actual list
+		expect(screen.getByRole('available-list').childElementCount).toBe(2);
+
+		await userEvent.clear(searchBar);
+		await userEvent.type(searchBar, 'Juego de Tronos');
+
+		// check counter
+		expect(screen.getByRole('available-books-counter').innerHTML).toBe('1');
+		// check the actual list
+		expect(screen.getByRole('available-list').childElementCount).toBe(1);
+		// check is that book in the list
+		expect(screen.getByRole('available-list').innerHTML).toContain('Juego de Tronos');
+
+	});
+
 	it('selects a genre', async () => {
 		// render app
 		render(<App />);
 
 		const genreSelector = screen.getByLabelText('Genre:');
 
-		fireEvent.change(genreSelector, { target: { value: 'Zombies' } });
+		await userEvent.selectOptions(genreSelector, 'Zombies');
 
 		// check if counter updated
 		expect(screen.getByRole('available-books-counter').innerHTML).toBe('1');
@@ -73,4 +98,13 @@ describe('available books filters test', async () => {
 		// only zombie book in mock
 		expect(screen.getByRole('available-list').innerHTML).toContain('Apocalipsis Zombie');
 	});
+
+	// it('select a min page of 50 and max page of 800', async () => {
+	// 	// render app
+	// 	render(<App />);
+
+	// 	const pagesSelector = screen.getByLabelText('Pages');
+
+	// 	fireEvent.change(pagesSelector, { target: { value: { min: 50, max: 800 } } });
+	// });
 });
